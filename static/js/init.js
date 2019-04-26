7a9fa0c3bc8e0ab01287e0957a180d1797c2090a
Sammy <sammy@Sammys-MacBook-Pro.local>
2019-04-26 08:50:43 +0530

    Implemeted carousel and transparent navbar

--- static/js/init.js
+++ static/js/init.js
@@ -1,3 +1,28 @@
+// Sidenav Implementation
 $(function() {
       $('.sidenav').sidenav();
     })
+
+
+// Carousel Implementation
+$('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+var autoplay = true;
+setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 4500);
+$('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
+
+
+
+$(document).ready(function(){
+
+      $(window).scroll(function(){
+
+       if($(window).scrollTop()>300){
+         $('nav').addClass('red');
+       }else{
+         $('nav').removeClass('red');
+       }
+    });
+  });
